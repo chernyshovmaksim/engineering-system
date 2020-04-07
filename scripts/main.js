5168cdf53f31e2eb31e35ae4ffdb3fc2e2aa13ad
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2020-04-07 13:19:26 +0500

    version 1

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    mmenuInit();
+});
+
+
+
+
+
+const mmenuInit = function(){
+    const menu = new MmenuLight(
+        document.querySelector( "#my-menu" )
+    );
+
+    const navigator = menu.navigation({
+        title: 'ООО "Инженерные системы"'
+    });
+    const drawer = menu.offcanvas();
+
+    document.querySelector( 'a[href="#my-menu"]' )
+        .addEventListener( 'click', ( evnt ) => {
+            evnt.preventDefault();
+            drawer.open();
+        });
+}
